5107e912fb6773130e1353a73a2c4a2baf0513bc
marciorodrigues87 <marcio.silva@grupozap.com>
2019-05-06 19:22:28 -0300

    simple csv file simulation

---
+++ src/gatling/scala/com/vivareal/search/simulation/GraylogSimpleRequestSimulation.scala
@@ -0,0 +1,37 @@
+package com.vivareal.search.simulation
+
+import com.typesafe.config.ConfigFactory.load
+import com.vivareal.search.config.S3Client.download
+import com.vivareal.search.config.GraylogClient.downloadCSV
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration._
+
+class GraylogSimpleRequestSimulation extends Simulation {
+
+  val config = load()
+
+  val baseURL = s"https://${config.getString("api.http.base")}"
+  val users = config.getInt("scenario.users")
+  val rampUp = config.getInt("gatling.rampUp")
+  val maxDuration = config.getInt("gatling.maxDuration")
+  val urisFile = "./src/gatling/resources/data/" + config.getString("graylog.urisFile")
+  val uriField = config.getString("graylog.uriField")
+
+  val httpConf = http.baseURL(baseURL)
+
+  downloadCSV(config.getString("graylog.query"), uriField, urisFile)
+  val feeder = csv(urisFile).circular
+
+  val scn = scenario("GraylogSimpleRequestSimulation").during(maxDuration seconds) {
+    feed(feeder)
+      .exec(http("GraylogSimpleRequests")
+      .get("${" + uriField + "}"))
+  }
+
+  setUp(scn.inject(rampUsers(users) over(rampUp seconds)))
+    .protocols(httpConf)
+    .maxDuration(maxDuration seconds)
+}
